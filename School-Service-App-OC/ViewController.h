f5f00f59b71eb3ec82107f48e62952ba9f05f470
Cc极生 <nkjay24@gmail.com>
2016-11-28 18:42:03 +0800

    Initial Commit

---
+++ School-Service-App-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  School-Service-App-OC
+//
+//  Created by NKjay on 2016/11/28.
+//  Copyright © 2016年 NKjay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ School-Service-App-OCTests/School_Service_App_OCTests.m
@@ -0,0 +1,39 @@
+//
+//  School_Service_App_OCTests.m
+//  School-Service-App-OCTests
+//
+//  Created by NKjay on 2016/11/28.
+//  Copyright © 2016年 NKjay. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface School_Service_App_OCTests : XCTestCase
+
+@end
+
+@implementation School_Service_App_OCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
