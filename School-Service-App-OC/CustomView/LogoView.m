313820c437d360a2223e59ebc96f6bed42bc98cf
Cc极生 <nkjay@outlook.com>
2017-01-22 11:12:01 +0800

    注册界面完成

---
+++ School-Service-App-OC/CustomView/LogoView.m
@@ -0,0 +1,21 @@
+//
+//  LogoView.m
+//  School-Service-App-OC
+//
+//  Created by NKJay on 2017/1/22.
+//  Copyright © 2017年 NKjay. All rights reserved.
+//
+
+#import "LogoView.h"
+
+@implementation LogoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
